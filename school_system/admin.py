282b4c7e854b3b67f2eb0a20bb4a3c02458047c1
Ray Gyapanin <72960660+Lixtic@users.noreply.github.com>
2026-01-13 23:31:23 +0000

    Remove hardcoded school names from admin headers

--- school_system/admin.py
+++ school_system/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Customize admin site headers
-admin.site.site_header = "Daboya Girls Model JHS Administration"
+admin.site.site_header = "School Portal Administration"
 admin.site.site_title = "School Admin"
-admin.site.index_title = "Welcome to Girls Model JHS Management System"
+admin.site.index_title = "Welcome to School Management System"
